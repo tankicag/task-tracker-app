6c847e1542b2e4e0f5d02854ddc2cb494c2a852f
tankicag <gonevatankica@gmail.com>
2021-08-19 22:19:34 +0100

    add tasks

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-button',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent implements OnInit {
+@Input() text: string;
+@Input() color: string;
+@Output() btnClick = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+onClick(){
+  this.btnClick.emit();
+}
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title: string = 'Task Tracker';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  toggleAddTask(){
+    console.log('toggle')
+  }
 }
