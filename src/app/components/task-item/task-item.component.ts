18f7ccd7a00d23a11a3eff1afad41077931cb12e
tankicag <gonevatankica@gmail.com>
2021-08-21 23:00:51 +0100

    add and delete tasks

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {Task} from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,12 +8,16 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
-@Input() task: Task;
-faTimes = faTimes;
+  @Input() task: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onDelete(task){
+    this.onDeleteTask.emit(task);
+  }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Task} from '../../Task'
-import {TASKS} from '../../mock-tasks'
+import { TaskService } from '../../service/task.service';
+import { Task } from '../../Task'
 
 @Component({
   selector: 'app-tasks',
@@ -8,10 +8,16 @@ import {TASKS} from '../../mock-tasks'
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-tasks: Task[]  = TASKS;
-  constructor() { }
+tasks: Task[]  = [];
+
+  constructor(private taskService: TaskService){ }
 
   ngOnInit(): void {
+
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
+  deleteTask(task:Task){
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter((t) => t.id !== task.id)));
+  }
 }
